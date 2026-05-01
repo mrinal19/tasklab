47fc5853e30d4372e7f3bd469e280c6579b99c1c
Mrinal Mishra <144545950+mrinal19@users.noreply.github.com>
2026-05-01 20:15:55 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -130,6 +130,8 @@ app.delete("/tasks/:id", auth, (req, res) => {
 });
 
 /* ---------- START ---------- */
-app.listen(5000, () => {
-  console.log("Server running on http://localhost:5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
 });
